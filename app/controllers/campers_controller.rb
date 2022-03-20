4303191847019a07e30f6f793549ce2b123539d5
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-03-20 16:37:22 -0700

    get methods built

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,2 +1,23 @@
 class CampersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+    def index
+        campers = Camper.all
+        render json: campers
+    end
+
+    def show
+        camper = Camper.find(params[:id])
+        render json: camper, serializer: CamperShowSerializer
+    end
+
+    def create
+        byebug
+    end
+
+    private
+
+    def render_not_found
+        render json: { error: "Camper not found"}, status: :not_found
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :signups, only: []
-  resources :campers, only: []
+  resources :campers, only: [:index, :show, :create]
+  # get "/camper", to: "campers#index"
+  # get "/campers/:id", to: "campers#show"
+  # post "/campers", to: "campers#create"
   resources :activities, only: []
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
