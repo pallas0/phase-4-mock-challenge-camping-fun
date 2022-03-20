42fd6d819a8685ea0ca4d33242f0e9e1b7e123ad
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-03-20 14:21:48 -0700

    models generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :signups, only: []
+  resources :campers, only: []
+  resources :activities, only: []
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
